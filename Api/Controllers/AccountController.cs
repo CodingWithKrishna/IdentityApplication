aebefe88cad36e3becbe446326fd4c8303e52d46
CodingWithKrishna <123403690+CodingWithKrishna@users.noreply.github.com>
2023-12-29 19:33:59 +0530

    Api Project Done

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using Api.DTOs.Account;
+using Api.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JwtService jwtService;
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+
+        public AccountController(JwtService jwtService,SignInManager<User> signInManager,UserManager<User> userManager)
+        {
+            this.jwtService = jwtService;
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        [Authorize]
+        [HttpGet("refresh-user-token")]
+        public async Task<ActionResult<UserDto>> RefreshToken()
+        {
+            var user = await userManager.FindByNameAsync(User.FindFirst(ClaimTypes.Email) ?.Value);
+            return  CreateApplicationUserDto(user);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await userManager.FindByNameAsync(model.UserName);
+            if(user == null)
+            {
+                return Unauthorized("Invalid username or password") ;
+            }
+            if(user.EmailConfirmed == false)
+            {
+                return Unauthorized("Please confirm your email");
+
+            }
+            var result = await signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Invalid Username Or Password");
+            }
+            return CreateApplicationUserDto(user); 
+        }
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            if(await CheckEmailExistAsync(model.Email))
+            {
+                return BadRequest($"An existing account is using {model.Email}, Emailaddress.Please try with another email address"); 
+            }
+            var userToAdd = new User
+            {
+                FirstName = model.FirstName.ToLower(),
+                LastName = model.LastName.ToLower(),
+                UserName = model.Email.ToLower(),
+                Email = model.Email.ToLower(),
+                EmailConfirmed =true
+            };
+            var result = await userManager.CreateAsync(userToAdd, model.Password);
+            if (!result.Succeeded) { return BadRequest(result.Errors); }
+            return Ok("Your account has been created , you can login");
+
+        }
+
+
+        private UserDto CreateApplicationUserDto(User user)
+        {
+            return new UserDto 
+            {
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            JWT = jwtService.CreateJWT(user)
+            };
+        }
+        private async Task<bool>CheckEmailExistAsync(string email)
+        {
+            return await userManager.Users.AnyAsync(u => u.Email == email.ToLower()); 
+        }
+       
+
+    }
+}
---
+++ Api/DTOs/Account/RegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.DTOs.Account
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(15,MinimumLength = 3,ErrorMessage ="First Name must be least (2), and maximum(1) character")]
+        public string? FirstName {  get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Last Name must be least (2), and maximum(1) character")]
+
+        public string? LastName { get; set; }
+        [Required]
+        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage ="Invalid Email Address")]
+        public string? Email { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "Password must be least (2), and maximum(1) character")]
+
+        public string? Password { get; set; }
+
+    }
+}
